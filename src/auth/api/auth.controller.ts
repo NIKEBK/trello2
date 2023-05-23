01136d15222ff176e089ece4000168bd67fe27d6
NIKEBK <nikebk4@gmail.com>
2023-05-23 14:40:40 +0600

    add:jwt-strategy,guards,test token authorization

--- src/auth/api/auth.controller.ts
+++ src/auth/api/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { CreateUserDTO } from 'src/user/api/user.dto';
 import { UserLoginDTO } from './auth.dto';
 import { AuthUserResponse } from '../../response/response';
+import { JwtAuthGuard } from 'src/guards/jwt-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -16,4 +17,11 @@ export class AuthController {
     login(@Body() body: UserLoginDTO): Promise<AuthUserResponse> {
         return this.AuthService.loginUser(body)
     }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('test')
+    test() {
+        return true
+    }
+
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './api/auth.controller';
 import { AuthService } from './service/auth.service';
 import { UserModule } from 'src/user/user.module';
 import { TokenModule } from '../token/token.module';
+import { JwtStrategy } from 'src/strategy';
 
 @Module({
     imports: [UserModule, TokenModule],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, JwtStrategy]
 })
 export class AuthModule { }
